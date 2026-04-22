b5fb7ea07bd9f5260ca87bd487acfe6eab95a232
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-23 03:29:42 +0600

    add naw inter active logine page naw and future new update

--- script/logine.js
+++ script/logine.js
@@ -1,4 +1,27 @@
 document.getElementById('logine-btn')
 .addEventListener('click', function () { 
-    console.log('Login button clicked');
+   //1 get the mobile number input 
+   const numberInput = document.getElementById('input-number');
+    const ConstNumber = numberInput.value;
+    console.log(ConstNumber);
+
+
+   // 2 get the pin input
+   const pinInput = document.getElementById('input-pin');
+   const ConstPin = pinInput.value;
+   console.log(ConstPin);
+
+    // 3 check the mobile number and pin
+    if(ConstNumber === '01700000000' && ConstPin === '1234'){
+         // 3-1 True    >> alert > home
+        alert('Login successful!')
+    }
+   else{
+       // 3-2 false   >> alert >  return to login page
+       alert("login failed")
+       return;
+
+   }
+
+
 })

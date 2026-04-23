08849a1c8d82a68bceeefc9e54243ecaad077477
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-23 22:45:02 +0600

    add cashout home page ,future update user active

---
+++ script/cashout.js
@@ -0,0 +1,15 @@
+document.getElementById('cashout-btn')
+.addEventListener('click', function () {
+    //1 get the agent number  and validate
+
+    //2 get the amount , validate , convert to number
+    // 3 get the current balance , validate , convert to number
+    //  4 calculate the new balance 
+    // 5 get the pin  and verify
+    //5-1 True: show an alert > set balance
+    //5-1 False: show an alert > return 
+
+
+
+
+})

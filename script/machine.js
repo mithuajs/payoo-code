b87f7665009c6420d0a21ef0d2a6f5bc08f86283
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-26 23:29:01 +0600

    add naw hidden and click naw show thish item naw only

--- script/machine.js
+++ script/machine.js
@@ -30,3 +30,20 @@ function SetBalance (value){
     balanceElement.innerText = value;
 
 }
+
+// machine id > hide all > show id 
+function ShowOnly(id){
+    console.log("show only click");
+    const addMoney = document.getElementById("add-money");
+     const Cashout = document.getElementById("cashout");
+//   console.log(`Add Money-${ addMoney}, Cash Out-${Cashout}`)
+// sobia k hide kore daw 
+addMoney.classList.add("hidden");
+Cashout.classList.add("hidden");
+
+// id wala element show koro
+
+const selected = document.getElementById(id);
+selected.classList.remove("hidden")
+    
+}

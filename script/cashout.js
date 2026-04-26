6d42c80e216991f9a1a46ab60a6d07278ec47a11
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-26 13:45:10 +0600

    new machine update

--- script/cashout.js
+++ script/cashout.js
@@ -1,49 +1,82 @@
 document.getElementById('cashout-btn')
     .addEventListener('click', function () {
-
-        //1 get the agent number  and validate
-        const CashoutAgentNumberInput = document.getElementById('agent-number');
-        const CashoutAgentNumber = CashoutAgentNumberInput.value;
-        console.log(CashoutAgentNumber);
+        const CashoutAgentNumber = getValueFromInput('agent-number');
         if(CashoutAgentNumber.length !== 11){
             alert("invalid agent number")
             return;
         }
+        //2 get the amount
+        const CashoutAmountString = getValueFromInput('amount');
 
-        //2 get the amount , validate , convert to number
-        const CashoutAmountInput = document.getElementById('amount');
-        const CashoutAmountString = CashoutAmountInput.value;
-        console.log(CashoutAmountString);
 
-        // 3 get the current balance , validate , convert to number
-
-        const balanceElement = document.getElementById('balance');
-        const balanceString = balanceElement.innerText;
-        console.log(balanceString);
+            const currentBalance = getBalance();
         //  4 calculate the new balance 
+        const newBalance = currentBalance - Number(CashoutAmountString);
 
-        const newBalance = Number(balanceString) - Number(CashoutAmountString);
-
+        console.log("New Balance: ", newBalance);
         if (newBalance < 0) {
             alert("invalid amount")
             return;
         }
 
-
         // 5 get the pin  and verify
-        const CashoutPinInput = document.getElementById('cashout-pin');
-        const pin = CashoutPinInput.value;
+
+        const pin = getValueFromInput('cashout-pin');
         if (pin === '1212') {
             alert("cashout successful")
             console.log("New Balance: ", newBalance);
-            balanceElement.innerText = newBalance;
-
+            document.getElementById("bl") = newBalance;
         } else {
             alert("invalid pin")
             return;
         }
-        //5-1 True: show an alert > set balance
-        //5-1 False: show an alert > return 
-
+       
 
     })
+
+//         //1 get the agent number  and validate
+//         const CashoutAgentNumberInput = document.getElementById('agent-number');
+//         const CashoutAgentNumber = CashoutAgentNumberInput.value;
+//         console.log(CashoutAgentNumber);
+//         if(CashoutAgentNumber.length !== 11){
+//             alert("invalid agent number")
+//             return;
+//         }
+
+//         //2 get the amount , validate , convert to number
+//         const CashoutAmountInput = document.getElementById('amount');
+//         const CashoutAmountString = CashoutAmountInput.value;
+//         console.log(CashoutAmountString);
+
+//         // 3 get the current balance , validate , convert to number
+
+//         const balanceElement = document.getElementById('balance');
+//         const balanceString = balanceElement.innerText;
+//         console.log(balanceString);
+//         //  4 calculate the new balance 
+
+//         const newBalance = Number(balanceString) - Number(CashoutAmountString);
+
+//         if (newBalance < 0) {
+//             alert("invalid amount")
+//             return;
+//         }
+
+
+//         // 5 get the pin  and verify
+//         const CashoutPinInput = document.getElementById('cashout-pin');
+//         const pin = CashoutPinInput.value;
+//         if (pin === '1212') {
+//             alert("cashout successful")
+//             console.log("New Balance: ", newBalance);
+//             balanceElement.innerText = newBalance;
+
+//         } else {
+//             alert("invalid pin")
+//             return;
+//         }
+//         //5-1 True: show an alert > set balance
+//         //5-1 False: show an alert > return 
+
+
+//     })
--- script/machine.js
+++ script/machine.js
@@ -1,7 +1,16 @@
 console.log( "machine added")
-// machine id > input value
-function getvaluefrominput(id){
+// machine id --> input value
+function getValueFromInput(id){
     const input = document.getElementById(id);
     const value = input.value;
     console.log( id ,value);
+    return value;
+}
+
+// machine --> balance 
+function getBalance(){
+const balanceElement = document.getElementById('balance');
+const balance = balanceElement.innerText;
+console.log (" current balance: ", Number(balance));
+return Number(balance);
 }

2795bbe111cd17b4f0ab165416be3ed3a87b0791
mithuajs <mdmobasherhossenmithu@gmail.com>
2026-04-27 15:15:05 +0600

    modify and update naw and some complete

--- script/logine.js
+++ script/logine.js
@@ -1,33 +1,33 @@
 document.getElementById('logine-btn')
-.addEventListener('click', function () { 
-   //1 get the mobile number input 
-   const numberInput = document.getElementById('input-number');
-    const ConstNumber = numberInput.value;
-    console.log(ConstNumber);
+    .addEventListener('click', function () {
+        //1 get the mobile number input 
+        const numberInput = document.getElementById('input-number');
+        const ConstNumber = numberInput.value;
+        console.log(ConstNumber);
 
 
-   // 2 get the pin input
-   const pinInput = document.getElementById('input-pin');
-   const ConstPin = pinInput.value;
-   console.log(ConstPin);
+        // 2 get the pin input
+        const pinInput = document.getElementById('input-pin');
+        const ConstPin = pinInput.value;
+        console.log(ConstPin);
 
-    // 3 check the mobile number and pin
-    if(ConstNumber === '01857810423' && ConstPin === '1212'){
-         // 3-1 True    >> alert > home
-        alert('Login successful!')
+        // 3 check the mobile number and pin
+        if (ConstNumber === '01857810423' && ConstPin === '1212') {
+            // 3-1 True    >> alert > home
+            alert('Login successful!')
 
-        // window.location.replace("/home.html");
-         window.location.assign("./home.html");
-
-
-
-    }
-   else{
-       // 3-2 false   >> alert >  return to login page
-       alert("login failed")
-       return;
-
-   }
+            // window.location.replace("/home.html");
+            window.location.assign("./home.html");
 
 
-})
+
+        }
+        else {
+            // 3-2 false   >> alert >  return to login page
+            alert("login failed")
+            return;
+
+        }
+
+
+    })
--- script/machine.js
+++ script/machine.js
@@ -1,49 +1,53 @@
-console.log( "machine added")
+console.log("machine added")
 // machine id --> input value
-function getValueFromInput(id){
+function getValueFromInput(id) {
     const input = document.getElementById(id);
     const value = input.value;
-    console.log( id ,value);
+    console.log(id, value);
     return value;
 }
 
 // machine --> balance 
-function getBalance(){
-const balanceElement = document.getElementById('balance');
-const balance = balanceElement.innerText;
-console.log (" current balance: ", Number(balance));
-return Number(balance);
+function getBalance() {
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    console.log(" current balance: ", Number(balance));
+    return Number(balance);
 }
 
 //add money --> balance
-function getaddMoneyBalance(){
+function getaddMoneyBalance() {
     const ADDbalanceElement = document.getElementById('amount-add');
     const ADDbalance = ADDbalanceElement.innerText;
-    console.log (" current balance: ", Number(amount-add));
-    return Number(amount-add);
+    console.log(" current balance: ", Number(amount - add));
+    return Number(amount - add);
 }
 
 
 // machine value --> balance
-function SetBalance (value){
+function SetBalance(value) {
     const balanceElement = document.getElementById('balance');
     balanceElement.innerText = value;
 
 }
 
 // machine id > hide all > show id 
-function ShowOnly(id){
+function ShowOnly(id) {
     console.log("show only click");
     const addMoney = document.getElementById("add-money");
-     const Cashout = document.getElementById("cashout");
-//   console.log(`Add Money-${ addMoney}, Cash Out-${Cashout}`)
-// sobia k hide kore daw 
-addMoney.classList.add("hidden");
-Cashout.classList.add("hidden");
+    const Cashout = document.getElementById("cashout");
 
-// id wala element show koro
 
-const selected = document.getElementById(id);
-selected.classList.remove("hidden")
-    
+    //   console.log(`Add Money-${ addMoney}, Cash Out-${Cashout}`)
+    // sobia k hide kore daw 
+    addMoney.classList.add("hidden");
+    Cashout.classList.add("hidden");
+
+
+
+    // id wala element show koro
+
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden")
+
 }
